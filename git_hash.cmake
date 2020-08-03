38ed4986d04e888fe520a09c9226d8469eb97f04
Matthew Elwin <elwin@northwestern.edu>
2020-08-02 23:59:36 -0500

    Working on target-specific hashing

--- git_hash.cmake
+++ git_hash.cmake
@@ -1,7 +1,9 @@
 find_package(Git REQUIRED)
 
+configure_file(${CMAKE_CURRENT_LIST_DIR}/git_hash.bash.in git_hash.bash @ONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/git_hash_target.bash.in git_hash_target.bash @ONLY)
+
 function(git_hash)
-  configure_file(${CMAKE_CURRENT_LIST_DIR}/git_hash.bash.in git_hash.bash)
    # Whenever make all (the default target) is built, update git_hash.h
    add_custom_target(git_hash ALL
                   COMMAND ${CMAKE_BINARY_DIR}/git_hash.bash ${CMAKE_BINARY_DIR}/git_hash.h
@@ -17,20 +19,18 @@ function(git_hash_target target)
   get_target_property(includes ${target} INCLUDE_DIRECTORIES)
 
   # append each library location to the list of files to hash
-  foreach(lib libs)
-    get_target_property(libpath ${lib} LOCATION)
-    list(APPEND files ${libpath})
+  foreach(lib ${libs})
+    list(APPEND files $<TARGET_FILE:${lib}>) 
   endforeach()
 
   # append each source file location to the list of files to hash
   list(APPEND files ${sources})
 
-  configure_file(${CMAKE_CURRENT_LIST_DIR}/git_hash_target.bash.in git_hash_target.bash)
-  add_custom_target(git_hash_target
+  add_custom_target(git_hash_${target}
     COMMAND ${CMAKE_BINARY_DIR}/git_hash_target.bash ${CMAKE_BINARY_DIR}/${target}_hash.h ${files} ${includes}
-    COMMEND "Updating ${target}_hash.h"
+    COMMENT "Updating ${target}_hash.h"
     BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/${target}_hash.h
     VERBATIM
     )
-  add_dependency(target git_hash_target)
+  add_dependencies(${target} git_hash_${target})
 endfunction()
