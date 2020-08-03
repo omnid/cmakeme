fd5caae09b67892e363e45758bac97a8a6f15ba7
Matthew Elwin <elwin@northwestern.edu>
2020-08-03 00:56:01 -0500

    git_hash target now takes include directories and filters the header files out of them.
    So the target hash is generated automatically from a target and includes the linked libraries, source files,
    and all the .h files in its include directories.

--- git_hash.cmake
+++ git_hash.cmake
@@ -14,20 +14,20 @@ function(git_hash)
 endfunction()
 
 function(git_hash_target target)
-  get_target_property(libs ${target} LINK_LIBRARIES)
-  get_target_property(sources ${target} SOURCES)
-  get_target_property(includes ${target} INCLUDE_DIRECTORIES)
-
+  get_target_property(githash_libs ${target} LINK_LIBRARIES)
+  get_target_property(githash_sources ${target} SOURCES)
+  get_target_property(githash_includes ${target} INCLUDE_DIRECTORIES)
+  get_target_property(githash_srcdir ${target} SOURCE_DIR)
   # append each library location to the list of files to hash
-  foreach(lib ${libs})
-    list(APPEND files $<TARGET_FILE:${lib}>) 
+  foreach(githash_lib ${githash_libs})
+    list(APPEND files $<TARGET_FILE:${githash_lib}>) 
   endforeach()
 
   # append each source file location to the list of files to hash
-  list(APPEND files ${sources})
+  list(APPEND files ${githash_sources})
 
   add_custom_target(git_hash_${target}
-    COMMAND ${CMAKE_BINARY_DIR}/git_hash_target.bash ${CMAKE_BINARY_DIR}/${target}_hash.h ${files} ${includes}
+    COMMAND ${CMAKE_BINARY_DIR}/git_hash_target.bash ${target} ${CMAKE_BINARY_DIR}/${target}_hash.h ${githash_files} ${githash_includes} ${githash_srcdir}/CMakeLists.txt
     COMMENT "Updating ${target}_hash.h"
     BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/${target}_hash.h
     VERBATIM
