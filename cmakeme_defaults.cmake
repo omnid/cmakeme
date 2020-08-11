bf43b61ca8b7b81c7201276f5fc5361f612fb9c0
Matthew Elwin <elwin@northwestern.edu>
2020-08-10 23:35:36 -0500

    Made defaults macros so settings affect global varisables

--- cmakeme_defaults.cmake
+++ cmakeme_defaults.cmake
@@ -1,15 +1,15 @@
 # Default cmake settings and options.
 # type - the default release type, None Debug Release RelWithDebInfo MinSizeRel
-function(cmakeme_defaults type)
+macro(cmakeme_defaults type)
   cmakeme_disable_in_source_builds()
   cmakeme_set_default_build_type(${type})
   cmakeme_no_lang_extensions()
-endfunction()
+endmacro()
 
 # set a default build type if not specified and store it in the cache
 # type - the build type to set
 # type - the default release type, None Debug Release RelWithDebInfo MinSizeRel
-function(cmakeme_set_default_build_type type)
+macro(cmakeme_set_default_build_type type)
   set(DEFAULT_BUILD_TYPE ${type})
   if(NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE ${DEFAULT_BUILD_TYPE} CACHE
@@ -19,7 +19,7 @@ function(cmakeme_set_default_build_type type)
     set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
       "Debug" "Release" "RelWithDebInfo" "MinSizeRel")
   endif()
-endfunction()
+endmacro()
 
 # disable in source builds. So if you try to configure a project
 # from within the source directory you will get an error.
@@ -34,8 +34,8 @@ function(cmakeme_disable_in_source_builds)
 endfunction()
 
 # disable c/c++ language extensions
-function(cmakeme_no_lang_extensions)
+macro(cmakeme_no_lang_extensions)
   set(CMAKE_CXX_EXTENSIONS OFF)
   set(CMAKE_C_EXTENSIONS OFF)
-endfunction()
+endmacro()
 
