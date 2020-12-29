184360f2d2bc7fee5cb6b31bbd683d417cb24b8a
Matthew Elwin <elwin@northwestern.edu>
2020-12-29 09:42:43 -0600

    Added Options to make documentation less cmake-specific.

--- sphinx/cmakeme_sphinx.cmake
+++ sphinx/cmakeme_sphinx.cmake
@@ -4,7 +4,7 @@
 # create sphinx documentation for the cmake files
 # CMAKEME_DOC_DIR - documentation for sphinx.  
 # Outputs the resulting documentation to build/html/cmake
-function(cmakeme_sphinx_cmake CMAKEME_DOC_DIR)
+function(cmakeme_sphinx_cmake doc_dir orgname org_url)
     find_program(SPHINX_EXECUTABLE
         NAMES sphinx-build
         DOC "Sphinx Documentation Builder (sphinx-doc.org)"
@@ -13,18 +13,19 @@ function(cmakeme_sphinx_cmake CMAKEME_DOC_DIR)
     if(NOT SPHINX_EXECUTABLE)
         message(FATAL_ERROR "SPHINX_EXECUTABLE (sphinx-build) is not found!")
     endif()
+    set(conf_homepage ${org_url})
+    set(conf_organization ${orgname})
     configure_file(${CMAKEME_ROOT_DIR}/sphinx/conf.py.in sphinx/conf.py)
 
     add_custom_command(OUTPUT html
         COMMAND ${SPHINX_EXECUTABLE}
         -b html
         -c ${CMAKE_BINARY_DIR}/sphinx
-        ${CMAKEME_DOC_DIR}
+        ${doc_dir}
         ${CMAKE_CURRENT_BINARY_DIR}/html/cmake
         > build-html.log
         COMMENT "sphinx-build html: see build-html.log"
         VERBATIM
         )
-  set_property(SOURCE html  PROPERTY SYMBOLIC 1) # html is not a real file
   add_custom_target(cmake_docs ALL DEPENDS html)
 endfunction()
