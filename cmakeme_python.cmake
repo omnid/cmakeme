5b1dabdaef58229086b223aaeeddd06a31070750
Matthew Elwin <elwin@northwestern.edu>
2022-06-05 14:34:43 -0500

    Introducing a method to deploy and build python projects.

---
+++ cmakeme_python.cmake
@@ -0,0 +1,54 @@
+#[=======================================================================[.rst:
+cmakeme_python
+----------------
+Manage and install python packages that are linked to C++ projects
+
+Use this module with ``find_package(cmakeme)``
+
+#]=======================================================================]
+
+
+
+
+#[=======================================================================[.rst:
+Commands
+^^^^^^^^
+    .. command:: cmakeme_python_package
+
+    Creates a python package installable from pip by invoking the appropriate setup.py
+
+        .. code-block:: cmake
+
+            cmakeme_python(setup directory pkgname)
+
+        ``directory``
+        The path to the directory containing setup.py/setup.cfg
+
+        ``pkgname``
+        The name of the python package
+#]=======================================================================]
+# TODO: this is deprecated so fix when we bump minimum cmake version
+find_package(PythonInterp)
+
+function(cmakeme_python directory pkgname)
+  if(PYTHONINTERP_FOUND)
+    # If we are in a virtual/conda environment
+    # (see https://www.scivision.dev/cmake-install-python-package/
+    # Accessed 06/05/2022
+    if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
+      set(_pip_args)
+    else()
+      set(_pip_args "--user")
+    endif()
+    # Build the wheel during code generation time
+    set(outdir "${CMAKE_BINARY_DIR}/${pkgname}/dist")
+    add_custom_command(OUTPUT ${outdir}
+      COMMAND ${PYTHON_EXECUTABLE}
+      ARGS -m build ${directory} --outdir ${outdir}
+      DEPENDS ${directory}
+      )
+    add_custom_target(${pkgname} ALL DEPENDS ${outdir})
+  else()
+    message(WARNING "Cannot cmakeme_python because Python Interpreter not found")
+  endif()
+endfunction()
---
+++ tests/cmake_python_test/README.md
@@ -0,0 +1,1 @@
+This is an example package used to test cmakeme's ability to aid in the creation and installation of python packages
