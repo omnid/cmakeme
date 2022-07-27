3bf5773d3d2e5d61d21f3eba3260f6b0deb904c8
Matthew Elwin <elwin@northwestern.edu>
2022-07-26 23:34:48 -0500

    The test of python_install now makes sure that the wheel is generated.

--- tests/run_tests.cmake
+++ tests/run_tests.cmake
@@ -5,3 +5,7 @@
 execute_process(COMMAND ${CMAKE_COMMAND} -B ${TEST_BIN_DIR} ${TEST_DIR} -DCMAKE_PREFIX_PATH=${CMAKEME_PATH} -DCMAKE_INSTALL_PREFIX=${TEST_BIN_DIR}/install)
 # Build and install the test project
 execute_process(COMMAND ${CMAKE_COMMAND} --build ${TEST_BIN_DIR} --target install)
+
+if(NOT EXISTS ${TEST_BIN_DIR}/dist/cmakeme_python_test-0.0.1-py3-none-any.whl)
+  message(FATAL_ERROR "Could not find ${TEST_BIN_DIR}/dist/cmakeme_python_test-0.0.1-py3-none-any.whl")
+endif()
