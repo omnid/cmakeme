e8cabb3ec4020724a23efa2f880219ae67badd1f
Matthew Elwin <elwin@northwestern.edu>
2026-05-12 16:21:38 -0500

    Pass some git_hash tests.

--- tests/githash_test/AdditionalTests.cmake
+++ tests/githash_test/AdditionalTests.cmake
@@ -2,3 +2,32 @@ message("githash_test AdditionalTests.cmake: Verifying Installation")
 verify_file_exists(${TEST_BIN_DIR}/install/include/githash_test/githash_test_exe_hash.h)
 verify_file_exists(${TEST_BIN_DIR}/install/include/githash_test/githash_test_lib_hash.h)
 verify_file_exists(${TEST_BIN_DIR}/install/include/githash_test/githash_test_git_hash.h)
+
+execute_process(COMMAND ${TEST_BIN_DIR}/install/bin/githash_test_exe
+  OUTPUT_VARIABLE hashes)
+
+find_package(Git REQUIRED)
+execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD OUTPUT_VARIABLE current_git_hash
+OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# split the output on newline
+string(REPLACE "\n" ";" hash_list ${hashes})
+list(GET hash_list 0 GIT_HASH_HEAD)
+list(GET hash_list 1 GIT_HASH_GITHASH_TEST_LIB)
+list(GET hash_list 2 GIT_HASH_GITHASH_TEST_EXE)
+
+if(NOT (GIT_HASH_HEAD STREQUAL current_git_hash))
+  message(FATAL_ERROR "GIT_HASH_HEAD is ${GIT_HASH_HEAD}, expected ${current_git_hash}")
+endif()
+
+set(expected_lib_hash "f8d51dd2f1aa346cc0f4413e8315ba189eb7829f")
+if(NOT (GIT_HASH_GITHASH_TEST_LIB STREQUAL expected_lib_hash))
+    message(FATAL_ERROR "GIT_HASH_GITHASH_TEST_LIB is ${GIT_HASH_GITHASH_TEST_LIB},
+ expected ${expected_lib_head}")
+endif()
+
+set(expected_exe_hash "f8d51dd2f1aa346cc0f4413e8315ba189eb7829f")
+if(NOT (GIT_HASH_GITHASH_TEST_LIB STREQUAL expected_exe_hash))
+    message(FATAL_ERROR "GIT_HASH_GITHASH_TEST_LIB is ${GIT_HASH_GITHASH_TEST_LIB},
+ expected ${expected_exe_head}")
+endif()
